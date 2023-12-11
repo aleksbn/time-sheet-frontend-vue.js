c0faf810bb0a4d73de91ad0e4f43da9935b1199e
Aleksandar Matić <aleksbn417@gmail.com>
2023-12-11 22:59:06 +0100

    Commit 41: User manual updated

--- src/pages/instructions/AboutUs.vue
+++ src/pages/instructions/AboutUs.vue
@@ -16,52 +16,50 @@
       <p>
         <a href="https://www.linkedin.com/in/aleksandar-matic-8a7b21120/" target="_blank"><img class="profile"
             src="../../media/images/profile.jpg" alt="" /></a>
-        Hello, everyone! My name is <b>Aleksandar Matic</b>, and I&#39;m
-        currently a high school professor of Information Technologies aiming to
-        start a new career path as a <b>web developer</b>. I graduated on
-        <b>Faculty of Information Technologies</b> on
-        <b>Slobomir P University</b> and have been in education department for
-        the last 12 years
-        <b>teaching programming languages, wed development, databases and a
-          little bit of web design</b>. I&#39;ve decided to switch my current career path to development, so,
-        here I am. My skills are both backend and frontend oriented, meaning
-        I&#39;m aiming to be a full stack developer. They include (but are not
-        limited to) the following technologies:
+        Hello, everyone! My name is <b>Aleksandar Matic</b>, and I&#39;m currently a high school professor of Information
+        Technologies aiming to develop my career as a <b>full stack developer</b>. I graduated on
+        <b>Faculty of Information Technologies</b> on <b>Slobomir P University</b> and have been in education department
+        for the last 12 years <b>teaching programming languages, wed development, databases and a little bit of web
+          design</b>. I&#39;ve decided to switch my current career path to development, so, here I am. My skills include
+        (but are not limited to) the following technologies:
       </p>
       <ol>
-        <li>
-          <b>C#</b>
-          <ol type="a">
-            <li>ASP.NET, ASP.NET core, ASP.NET MVC, Web API</li>
-            <li>WPF, MVVM, .NET6</li>
-            <li>RazorPages</li>
-          </ol>
+        <li>Backend <ul>
+            <li>Node.js</li>
+            <li>C#</li>
+            <li>.NET</li>
+            <li>ASP.NET Web API</li>
+            <li>ASP.NET MVC</li>
+          </ul>
         </li>
-        <li>
-          <b>JavaScript</b>
-          <ol type="a">
-            <li>Vanilla JS, ES6</li>
-            <li>TypeScript</li>
-            <li>Vue JS</li>
-          </ol>
+        <li>Databases <ul>
+            <li>T-SQL</li>
+            <li>MS SQL server</li>
+            <li>MongoDB</li>
+          </ul>
         </li>
-        <li><b>TSQL</b></li>
-        <li>
-          <b>Others</b>
-          <ol type="a">
-            <li>HTML, CSS, Bootstrap, REST, Git, JSON…</li>
-          </ol>
+        <li>Frontend <ul>
+            <li>JavaScript</li>
+            <li>Vue.js</li>
+            <li>React.js</li>
+            <li>Razor Pages</li>
+            <li>WPF (XAML)</li>
+          </ul>
+        </li>
+        <li>Less relevant or uncategorized skills <ul>
+            <li>GIT, HTML, CSS, JWT, Linq, JSON, Entity Framework...</li>
+          </ul>
         </li>
       </ol>
       <p>
-        Now, let&#39;s not make this longer than it needs to be. In case you
-        need some other info or contant data, feel free to visit my
+        Now, let&#39;s not make this longer than it needs to be. In case you need some other info or contant data, feel
+        free to visit my
         <a href="https://www.linkedin.com/in/aleksandar-matic-8a7b21120/" target="_blank">LinkedIn profile</a>. Let&#39;s
         get to the instruction itself, shall we?
       </p>
       <p>
-        Before I dive into installation, let me briefly explain technologies
-        used in this project for my potential future employees:
+        Before I dive into installation, let me briefly explain technologies used in this project for my potential future
+        employees:
       </p>
       <ol>
         <li>
@@ -91,13 +89,13 @@
         <li>
           <b>Database</b>
           <ol type="a">
-            <li>TSQL server</li>
+            <li>T-SQL server</li>
           </ol>
         </li>
       </ol>
       <p>Proceed to:
         <router-link to="/installationmanual">
-          <h3>Required software installation guide, if you want to use it soley on your own computer</h3>
+          <h3>Required software installation guide in more details</h3>
         </router-link>
         <router-link to="/usermanual">
           <h3>Application user manual</h3>
